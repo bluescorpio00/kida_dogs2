12526bec94b8d3aee74f01d8204dfd4bc43166f8
Aleksandra <ola.kida10@o2.pl>
2023-09-30 21:40:42 +0200

    Implement adding activities

--- lib/app/home/add_activities/add_activities_page_content.dart
+++ lib/app/home/add_activities/add_activities_page_content.dart
@@ -1,14 +1,41 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class AddActivitiesPageContent extends StatelessWidget {
+class AddActivitiesPageContent extends StatefulWidget {
   const AddActivitiesPageContent({
     super.key,
   });
 
   @override
+  State<AddActivitiesPageContent> createState() =>
+      _AddActivitiesPageContentState();
+}
+
+class _AddActivitiesPageContentState extends State<AddActivitiesPageContent> {
+  var activityName = '';
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('dwa'),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextField(
+            decoration: const InputDecoration(hintText: 'Nazwa aktywności'),
+            onChanged: (newValue) {
+              setState(() {
+                activityName = newValue;
+              });
+            },
+          ),
+          ElevatedButton(
+              onPressed: () {
+                FirebaseFirestore.instance
+                    .collection('Activities')
+                    .add({'name': activityName, 'number': 4});
+              },
+              child: const Text('Dodaj')),
+        ],
+      ),
     );
   }
 }
